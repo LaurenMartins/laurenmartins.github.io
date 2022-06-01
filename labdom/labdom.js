5790d3e16cbe458ab83246a150a95998dc8d7683
Your Name <you@example.com>
2022-06-01 13:17:41 -0300

    lab1

---
+++ labdom/labdom.js
@@ -0,0 +1,9 @@
+const num1 = document.getElementById("n1");
+const num2 = document.getElementById("n2");
+const result = document.getElementById("button").addEventListener("click", soma);
+
+function soma(num1, num2)
+{
+    return num1 + num2;
+}
+
